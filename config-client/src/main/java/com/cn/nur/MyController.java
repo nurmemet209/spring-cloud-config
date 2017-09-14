be7728db0bd2432fa49acbf5180bbc0e704be486
nurmemet <2335515050@qq.com>
2017-09-14 18:22:28 +0800

    first

---
+++ config-client/src/main/java/com/cn/nur/MyController.java
@@ -0,0 +1,25 @@
+package com.cn.nur;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by nurmemet on 9/12/2017.
+ */
+@RestController
+public class MyController {
+
+    @Value("${message}")
+    String msg;
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello "+ msg;
+    }
+
+
+
+
+
+}
